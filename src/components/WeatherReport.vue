b7d707129870237de807d3c658bf81a74d901c2b
Paulius G <paulius@2simple.com>
2024-03-23 13:05:28 +0000

    Update index.html and WeatherReport.vue with styles

--- src/components/WeatherReport.vue
+++ src/components/WeatherReport.vue
@@ -49,8 +49,18 @@ onMounted(async () => {
 
 <template>
     <div>
-        <article v-if="data && data.current">
-            {{ data.current }}
+        <article v-if="data && data.current" class="max-w-md w-96 rounded-lg shadow-lg p-4 flex bg-white text-black">
+            <div class="basis-1/4 text-left">
+                <img :src="data.current.condition.icon" class="h-16 w-16" />
+            </div>
+            <div class="basis-3/4 text-left">
+                <h1 class="text-3xl font-bold">
+                    {{ data.current.condition.text }}
+                    <span class="text-2xl block">{{ data.current.temp_c }}&#8451;</span>
+                </h1>
+                <p>{{ data.location.name }} {{ data.location.region }}</p>
+                <p>Precipitation: {{ data.current.precip_mm }}mm</p>
+            </div>
         </article>
         <div v-else>Loading...</div>
     </div>

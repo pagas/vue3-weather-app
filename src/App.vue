b8e565741a50df1480b80354b2e261d89da9bc80
Paulius G <paulius@2simple.com>
2024-03-23 17:19:13 +0000

    Refactor UI layout and styles

--- src/App.vue
+++ src/App.vue
@@ -14,36 +14,13 @@ const onCoordsLoaded = (_coords: Coords) => {
 </script>
 
 <template>
-  <GetLocation @coordsLoaded="onCoordsLoaded" />
-  <WeatherReport v-if="coords" :coords="coords" />
-  <div v-else>Loading...</div>
+  <div
+    class="bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 w-full h-screen flex flex-col items-center justify-center">
+    <GetLocation @coordsLoaded="onCoordsLoaded" />
+    <WeatherReport v-if="coords" :coords="coords" />
+    <div v-else>Loading coordinates...</div>
+  </div>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
